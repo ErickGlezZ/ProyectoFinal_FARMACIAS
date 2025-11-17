6fb56e829d002e85bd0ddf1efb4db990054a8766
Erick González <erickgoonzalez5@gmail.com>
2025-11-17 12:00:45 -0600

    Agregue parte de la estrcutura principal y codigo para abrir la ventana principal

--- src/main/java/com/mycompany/proyectofinal_farmacias/ProyectoFinal_FARMACIAS.java
+++ src/main/java/com/mycompany/proyectofinal_farmacias/ProyectoFinal_FARMACIAS.java
@@ -12,5 +12,7 @@ public class ProyectoFinal_FARMACIAS {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        
+        new VentanaInicio();
     }
 }
